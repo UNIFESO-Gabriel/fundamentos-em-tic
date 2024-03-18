49c7cef84271153fcf20f4b1ec51f64a95fdf9cb
Gabriel Machado <gabriel.rmachado10@gmail.com>
2024-03-18 11:01:41 -0300

    Resolução dos exercícios de decisão - Aula 02

---
+++ Codigos/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int soma_digitos(int n) {
+
+    if (n == 0)
+        return 0;
+    else
+        return n % 10 + soma_digitos(n / 10);
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", soma_digitos(n));
+    return 0;
+}
