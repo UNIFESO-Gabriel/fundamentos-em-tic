ffe7376cad6ed2e1458054746fc907fcece32ff4
Gabriel Machado <gabriel.rmachado10@gmail.com>
2024-04-06 14:06:01 -0300

    Correção de bug em 'desconto_produto.c'

--- Codigos/Slide02_Sequencia/desconto_produto.c
+++ Codigos/Slide02_Sequencia/desconto_produto.c
@@ -8,10 +8,10 @@ int main() {
 	printf("Digite o valor inicial do produto (R$): ");
 	scanf("%f", &valor_inicial);
 	
-	printf("Digite o valor do desconto: ");
+	printf("Digite o valor do desconto (em %): ");
 	scanf("%d", &desconto);
 	
-	float desc_decimal = ((float)desconto) / 100.0;
+	float desc_decimal = 1 - (((float)desconto) / 100.0);
 	valor_final = valor_inicial * desc_decimal;
 	
 	printf("O valor do produto com desconto eh R$ %.2f\n", valor_final);
